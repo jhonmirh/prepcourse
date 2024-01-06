445bb64f0640de535f88c33625f94864dd4e1e57
jhonmirh <jhonmirh@gmail.com>
2024-01-06 14:31:30 -0400

    PRUEBA CHALLENGE

---
+++ 02.js
@@ -0,0 +1,30 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function stringMasLarga(strings) {
+  // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
+  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+  // Ej:
+  // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
+  // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
+
+  // Tu código aca
+
+  let larga = strings[0];
+  for (let index = 1; index < strings.length; index++) {
+    if (larga.length <= strings[index].length) {
+      larga = strings[index]
+    }
+  }
+
+  return larga;
+}
+
+
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = stringMasLarga
